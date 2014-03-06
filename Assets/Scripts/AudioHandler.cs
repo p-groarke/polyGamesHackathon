713f609b7880ca45ade33a1f0d2d4f83e7484b21
Julien Aymong <julien.aymong@gmail.com>
2014-03-06 06:10:01 -0500

    Add AudioHandler for hit sounds

---
+++ Assets/Scripts/AudioHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+
+using System.Collections;
+
+
+
+public class AudioHandler : MonoBehaviour {
+
+	// Static reference to singleton object
+	public static AudioHandler instance;
+	public AudioClip[] myClip;
+	
+	// Use this for initialization
+	
+	void Start ()
+	{
+		instance = gameObject.GetComponent<AudioHandler>();
+	}
+
+	public void playSound ()
+	{
+		audio.PlayOneShot(myClip[Random.Range(0,myClip.Length)]);
+	}
+	
+	
+}
