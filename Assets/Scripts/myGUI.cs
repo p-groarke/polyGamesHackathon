d403b580779b5124dba1d20bca3ab6a1b98f17fa
Marie-France Miousse <mfmiousse@gmail.com>
2014-03-06 12:20:13 -0500

    bigger buttons

--- Assets/Scripts/myGUI.cs
+++ Assets/Scripts/myGUI.cs
@@ -13,13 +13,13 @@ public class myGUI : MonoBehaviour {
 	// Use this for initialization
 	void OnGUI () {
 		GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);  //Keeps the button looking like a button
-		buttonStyle.fontSize = 30;  //changes font size of button
+		buttonStyle.fontSize = 40;  //changes font size of button
 		buttonStyle.normal.textColor = Color.white;
 		buttonStyle.fontStyle = FontStyle.Bold;
-		if (GUI.Button (new Rect (Screen.width/2 - 75,Screen.height / 2 - 150,150,100), "Play", buttonStyle)) {
+		if (GUI.Button (new Rect (Screen.width/2 - 150,Screen.height / 2 - 150,300,100), "Play", buttonStyle)) {
 			Application.LoadLevel ("Level1"); 
 		}
-		if(GUI.Button (new Rect (Screen.width/2 - 75,Screen.height / 2 + 50,150,100), "Quit", buttonStyle)) {
+		if(GUI.Button (new Rect (Screen.width/2 - 150,Screen.height / 2 + 50,300,100), "Quit", buttonStyle)) {
 			Application.Quit();
 		}
 	}
