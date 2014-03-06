6beaed30c72af99f6e89e8fd5b57b7ca4352a7e3
Marie-France Miousse <mfmiousse@gmail.com>
2014-03-05 19:03:24 -0500

    MainMenu button working

--- Assets/Scripts/myGUI.cs
+++ Assets/Scripts/myGUI.cs
@@ -13,10 +13,10 @@ public class myGUI : MonoBehaviour {
 	// Use this for initialization
 	void OnGUI () {
 		if (GUI.Button (new Rect (Screen.width/2 - 75,Screen.height / 2 - 150,150,100), "Play")) {
-			print ("You clicked the Play button!");
+			Application.LoadLevel ("Level1"); 
 		}
 		if(GUI.Button (new Rect (Screen.width/2 - 75,Screen.height / 2 + 50,150,100), "Quit")) {
-			print ("You clicked the Quit button!");
+			Application.Quit();
 		}
 	}
 
